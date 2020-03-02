70722b9124f225dfb07f6f69420b3b0cf3a3efb4
Jin Kim <jinkim172@gmail.com>
2020-03-01 22:07:57 -0500

    Test Calculator I.

---
+++ testcalculator/testcalculator/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  testcalculator
+//
+//  Created by Jin U Kim on 3/1/20.
+//  Copyright © 2020 Jin U Kim. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
